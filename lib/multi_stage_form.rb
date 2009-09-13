d49789a32eef4e12fa26d2a76a5b0f0e093773ee
seeweer <seeweer@gmail.com>
2009-09-13 16:21:44 +0200

    Improved controller configuration

--- lib/multi_stage_form.rb
+++ lib/multi_stage_form.rb
@@ -1,39 +1,39 @@
 module MultiStageForms
   module ActiveRecord
-    def self.included( base )
-      base.extend( ClassMethods )  
+    def self.included(base)
+      base.extend(ClassMethods)
     end
 
     module ClassMethods
-      def session=( s )
-        @session = s
-      end
-
-      def multi_stage_form( options = {} ) 
+      def multi_stage_form(options = {})
         self.validates_on_stage = options[:validates_on_stage] || []
         self.stages = options[:stages] || 0
       end
 
+      def partial_data
+        @partial_data
+      end
+
+      def partial_data= pd
+        @partial_data = pd
+      end
+
       def get_partial_data_from_session
-        unless( @session[:partial_data].nil? )
-          data = @session[:partial_data] 
+        unless(partial_data.blank?)
+          data = partial_data
         else
           data = self.new
         end
         data
       end
 
-      def partial_create( params )
+      def partial_create(params)
         model = self.get_partial_data_from_session
         model.attributes = params
         model
       end
       
-      def clear_session
-        @session[:partial_data] = nil
-      end
-
-      def stages=( s )
+      def stages=(s)
         @@stages = s
       end
 
@@ -41,7 +41,7 @@ module MultiStageForms
         @@stages
       end
 
-      def validates_on_stage=( v )
+      def validates_on_stage=(v)
         @@validates_on_stage = v
       end
 
@@ -52,33 +52,61 @@ module MultiStageForms
   end
 
   module ActionController
-    def self.included( base )
-      base.send( :include, InstanceMethods )
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def multi_stage_form(options = {})
+      end
     end
 
     module InstanceMethods
+      def msf_new(current_step)
+        current_step = current_step || 1
+        if(current_step.to_i == 1)
+          session[:partial_data] = nil
+        end
+        model_class_name = controller_name.singularize.camelize
+        model = Kernel.const_get(model_class_name)
+        model.partial_data = session[:partial_data]
+        model_instance = model.get_partial_data_from_session
+        model_instance.current_stage = current_step
+        model_instance
+      end
 
-      def multi_stage_form( model, options = {} )
+      def msf_create(model_attr)
+        model_class_name = controller_name.singularize.camelize
+        model = Kernel.const_get(model_class_name)
+        model.partial_data = session[:partial_data]
+        model_instance = model.partial_create(model_attr)
+        model_instance.current_stage = model_attr[:current_stage]
+        model_instance
+      end
+
+      def multi_stage_form(model, options = {})
         stage_name = "stage_#{model.current_stage}"
         class_name = model.class.to_s.downcase
-        self.send( stage_name ) if( respond_to?( stage_name ) )
+        self.send(stage_name)if(respond_to?(stage_name))
 
-        validates = model.class.validates_on_stage[model.current_stage] || []
+        validates = model.class.validates_on_stage[model.current_stage.to_i] || []
 
-        if( valid_for_attributes( model, validates ) )
-          unless( model.nil? ) 
+        
+        if(valid_for_attributes(model, validates))
+          unless(model.nil?)
             session[:partial_data] = model
           end
-          unless( model.current_stage.to_i == model.class.stages.to_i )
-            redirect_to( eval( "new_#{class_name}_path( :current_stage => model.current_stage.to_i + 1 )" ) )
+          unless(model.current_stage.to_i == model.class.stages.to_i)
+            redirect_to(eval("new_#{class_name}_path(:current_stage => model.current_stage.to_i + 1)"))
           else
             model = get_partial_data_from_session
             model.save
             session[:partial_data] = nil
-            redirect_to( eval( "#{class_name.pluralize}_path" ) )
+            redirect_to(eval("#{class_name.pluralize}_path"))
           end
         else
-          render( :action => "new" )
+          render(:action => "new")
         end
       end
 
@@ -87,7 +115,7 @@ module MultiStageForms
       end
 
       def get_partial_data_from_session
-        unless( session[:partial_data].nil? )
+        unless(session[:partial_data].nil?)
           data = session[:partial_data] 
         else
           data = self.new
@@ -95,19 +123,23 @@ module MultiStageForms
         data
       end
 
-      def valid_for_attributes( model, attributes )
-        unless( model.valid? )
+      def valid_for_attributes(model, attributes)
+        puts "DDDDDDDDDDDDDD"
+        puts model.valid?.inspect
+        puts attributes.inspect
+
+        unless(model.valid?)
           attributes = attributes.map{ |x| x.to_s }
           errors = model.errors
           our_errors = []
           errors.each do |attr, error|
-            if( attributes.include?( attr ) )
+            if(attributes.include?(attr))
               our_errors << [attr, error]
             end
           end
           errors.clear
           our_errors.each do |attr, error|
-            errors.add( attr, error )
+            errors.add(attr, error)
           end
           return false unless errors.empty?
         end
